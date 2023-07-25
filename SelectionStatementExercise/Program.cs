4c85a721eb9c26f4bd7e541d510d0b82f8cb928e
Cali Weber <calista.weber1@gmail.com>
2023-07-25 07:16:23 -0700

    finished exercise

--- SelectionStatementExercise/Program.cs
+++ SelectionStatementExercise/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var r = new Random();
+            int FaveNumber = r.Next(1, 20);
+            Console.WriteLine("enter a number between 1 and 20");
+
+            var userInput = int.Parse(Console.ReadLine());
+            if (userInput > FaveNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userInput<FaveNumber)
+            { 
+                Console.WriteLine("Too Low"); 
+            }
+            else
+            {
+                Console.WriteLine("Correct, that is the number");
+            }
+
         }
     }
 }
